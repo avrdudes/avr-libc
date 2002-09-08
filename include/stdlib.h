a6ab48e24de290cb43b5539bd05cb77556b3f509
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2002-09-08 22:01:42 +0000

    Document <stdlib.h>.  Much of the documentation has been adapted from the FreeBSD manual, so copyright of the Regents of the UCB added.
    Move the bit definitions of dtostre() into this file, and rename them
    to start with DTOSTRE_.
    
    Add CVS Id.
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@88 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,4 +1,8 @@
 /* Copyright (c) 2002, Marek Michalkiewicz
+
+   Portions of documentation Copyright (c) 1990, 1991, 1993, 1994
+   The Regents of the University of California.
+
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
@@ -21,10 +25,15 @@
   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  POSSIBILITY OF SUCH DAMAGE. */
+  POSSIBILITY OF SUCH DAMAGE.
+
+  $Id$
+*/
 
 #ifndef _STDLIB_H_
 #define	_STDLIB_H_ 1
+
+#ifndef __ASSEMBLER__
 
 #define __need_NULL
 #define __need_size_t
@@ -39,19 +48,33 @@
 extern "C" {
 #endif
 
+/** \defgroup avr_stdlib General utilities
+    \code #include <stdlib.h> \endcode
+
+    This file declares some basic C macros and functions as
+    defined by the ISO standard, plus some AVR-specific extensions.
+*/
+
+/*@{*/
+/** \name Standard functionality. */
+/** Result type for function div(). */
 typedef struct {
 	int quot;
 	int rem;
 } div_t;
 
+/** Result type for function ldiv(). */
 typedef struct {
 	long quot;
 	long rem;
 } ldiv_t;
 
+/** Comparision function type for qsort(), just for convenience. */
 typedef int (*__compar_fn_t)(const void *, const void *);
 
 #define	RAND_MAX 0x7FFFFFFF
+
+#ifndef DOXYGEN
 
 #ifndef __ATTR_CONST__
 #define __ATTR_CONST__ __attribute__((__const__))
@@ -69,89 +92,396 @@ typedef int (*__compar_fn_t)(const void *, const void *);
 #define __ATTR_PURE__ __attribute__((__pure__))
 #endif
 
+#endif
+
 extern __inline__ void abort(void) __ATTR_NORETURN__;
+
+/** The abort() function causes abnormal program termination to occur.
+    In the limited AVR environment, execution is effectively halted
+    by entering an infinite loop. */
 extern __inline__ void
 abort(void)
 {
 	for (;;);
 }
 
-extern __inline__ int abs(int __x) __ATTR_CONST__;
-extern __inline__ int
-abs(int __x)
-{
-	return (__x < 0) ? -__x : __x;
-}
+/** The abs() function computes the absolute value of the integer \c i.
+   \note The abs() and labs() functions are builtins of gcc.
+*/
+extern int abs(int __i) __ATTR_CONST__;
+#ifndef DOXYGEN
+#define	abs(__i) __builtin_abs(__i)
+#endif
 
-extern __inline__ long labs(long __x) __ATTR_CONST__;
-extern __inline__ long
-labs(long __x)
-{
-	return (__x < 0) ? -__x : __x;
-}
+/** The labs() function computes the absolute value of the long integer
+    \c i.
+   \note The abs() and labs() functions are builtins of gcc.
+*/
+extern long labs(long __i) __ATTR_CONST__;
+#ifndef DOXYGEN
+#define labs(__i) __builtin_labs(__i)
+#endif
 
-extern void *bsearch(const void *, const void *, size_t, size_t,
-	int (*)(const void *, const void *));
+/**
+     The bsearch() function searches an array of \c nmemb objects, the
+     initial member of which is pointed to by \c base, for a member
+     that matches the object pointed to by \c key.  The size of each
+     member of the array is specified by \c size.
+
+     The contents of the array should be in ascending sorted order
+     according to the comparison function referenced by \c compar.
+     The \c compar routine is expected to have two arguments which
+     point to the key object and to an array member, in that order,
+     and should return an integer less than, equal to, or greater than
+     zero if the key object is found, respectively, to be less than,
+     to match, or be greater than the array member.
+
+     The bsearch() function returns a pointer to a matching member of
+     the array, or a null pointer if no match is found.  If two
+     members compare as equal, which member is matched is unspecified.
+*/
+extern void *bsearch(const void *__key, const void *__base, size_t __nmemb,
+		     size_t size, int (*__compar)(const void *, const void *));
 
 /* __divmodhi4 and __divmodsi4 from libgcc.a */
-extern div_t div(int, int) __asm__("__divmodhi4") __ATTR_CONST__;
-extern ldiv_t ldiv(long, long) __asm__("__divmodsi4") __ATTR_CONST__;
+/**
+     The div() function computes the value \c num/denom and returns
+     the quotient and remainder in a structure named \c div_t that
+     contains two int members named \c quot and \c rem.
+*/
+extern div_t div(int __num, int __denom) __asm__("__divmodhi4") __ATTR_CONST__;
+/**
+     The ldiv() function computes the value \c num/denom and returns
+     the quotient and remainder in a structure named \c ldiv_t that
+     contains two long integer members named \c quot and \c rem.
+*/
+extern ldiv_t ldiv(long __num, long __denom) __asm__("__divmodsi4") __ATTR_CONST__;
 
-extern void qsort(void *, size_t, size_t, __compar_fn_t);
+/**
+     The qsort() function is a modified partition-exchange sort, or
+     quicksort.
 
-extern long strtol(const char *, char **, int);
-extern unsigned long strtoul(const char *, char **, int);
+     The qsort() function sort an array of \c nmemb objects, the
+     initial member of which is pointed to by \c base.  The size of
+     each object is specified by \c size.  The contents of the array
+     base are sorted in ascending order according to a comparison
+     function pointed to by \c compar, which requires two arguments
+     pointing to the objects being compared.
 
-extern __inline__ long atol(const char *) __ATTR_PURE__;
+     The comparison function must return an integer less than, equal
+     to, or greater than zero if the first argument is considered to
+     be respectively less than, equal to, or greater than the second.
+*/
+extern void qsort(void *__base, size_t __nmemb, size_t __size,
+		  __compar_fn_t __compar);
+
+/**
+    The strtol() function converts the string in \c nptr to a long
+    value.  The conversion is done according to the given base, which
+    must be between 2 and 36 inclusive, or be the special value 0.
+
+    The string may begin with an arbitrary amount of white space (as
+    determined by isspace()) followed by a single optional `+' or `-'
+    sign.  If \c base is zero or 16, the string may then include a
+    ``0x'' prefix, and the number will be read in base 16; otherwise,
+    a zero base is taken as 10 (decimal) unless the next character is
+    `0', in which case it is taken as 8 (octal).
+
+    The remainder of the string is converted to a long value in the
+    obvious manner, stopping at the first character which is not a
+    valid digit in the given base.  (In bases above 10, the letter `A'
+    in either upper or lower case represents 10, `B' represents 11,
+    and so forth, with `Z' representing 35.)
+
+    If \c endptr is not NULL, strtol() stores the address of the first
+    invalid character in \c *endptr.  If there were no digits at all,
+    however, strtol() stores the original value of \c nptr in \c
+    *endptr.  (Thus, if \c *nptr is not `\0' but \c **endptr is `\0'
+    on return, the entire string was valid.)
+
+    The strtol() function returns the result of the conversion, unless
+    the value would underflow or overflow.  If no conversion could be
+    performed, 0 is returned.  If an overflow or underflow occurs, \c
+    errno is set to ERANGE and the function return value is clamped to
+    \c LONG_MIN or \c LONG_MAX, respectively.
+*/
+extern long strtol(const char *__nptr, char **__endptr, int __base);
+
+/**
+    The strtoul() function converts the string in \c nptr to an
+    unsigned long value.  The conversion is done according to the
+    given base, which must be between 2 and 36 inclusive, or be the
+    special value 0.
+
+    The string may begin with an arbitrary amount of white space (as
+    determined by isspace()) followed by a single optional `+' or `-'
+    sign.  If \c base is zero or 16, the string may then include a
+    ``0x'' prefix, and the number will be read in base 16; otherwise,
+    a zero base is taken as 10 (decimal) unless the next character is
+    `0', in which case it is taken as 8 (octal).
+
+    The remainder of the string is converted to an unsigned long value
+    in the obvious manner, stopping at the first character which is
+    not a valid digit in the given base.  (In bases above 10, the
+    letter `A' in either upper or lower case represents 10, `B'
+    represents 11, and so forth, with `Z' representing 35.)
+
+    If \c endptr is not NULL, strtol() stores the address of the first
+    invalid character in \c *endptr.  If there were no digits at all,
+    however, strtol() stores the original value of \c nptr in \c
+    *endptr.  (Thus, if \c *nptr is not `\0' but \c **endptr is `\0'
+    on return, the entire string was valid.)
+
+    The strtoul() function return either the result of the conversion
+    or, if there was a leading minus sign, the negation of the result
+    of the conversion, unless the original (non-negated) value would
+    overflow; in the latter case, strtoul() returns ULONG_MAX, and \c
+    errno is set to ERANGE.  If no conversion could be performed, 0 is
+    returned.
+*/
+extern unsigned long strtoul(const char *__nptr, char **__endptr, int __base);
+
+/**
+    The atol() function converts the initial portion of the string
+    pointed to by \c nptr to long integer representation.
+
+    It is equivalent to:
+
+           \code strtol(nptr, (char **)NULL, 10); \endcode
+*/
+extern __inline__ long atol(const char *nptr) __ATTR_PURE__;
 extern __inline__ long
-atol(const char *__p)
+atol(const char *__nptr)
 {
-	return strtol(__p, (char **) 0, 10);
+	return strtol(__nptr, (char **) 0, 10);
 }
 
-#if 0
-extern __inline__ int atoi(const char *__p) __ATTR_PURE__;
+/**
+    The atoi() function converts the initial portion of the string
+    pointed to by \c nptr to integer representation.
+
+    It is equivalent to:
+
+           \code (int)strtol(nptr, (char **)NULL, 10); \endcode
+*/
+#ifdef DOXYGEN
+extern __inline__ int atoi(const char *__nptr) __ATTR_PURE__;
 extern __inline__ int
-atoi(const char *__p)
+atoi(const char *__nptr)
 {
-	return (int) atol(__p);
+	return (int) atol(__nptr);
 }
 #else
 /* optimized asm version, much smaller if strtol() not used elsewhere */
 extern int atoi(const char *) __ATTR_PURE__;
 #endif
 
-extern void exit(int) __ATTR_NORETURN__;
+/**
+   The exit() function terminates the application.  Since there is no
+   environment to return to, \c status is ignored, and code execution
+   will eventually reach an infinite loop, thereby effectively halting
+   all code processing.
 
-extern void *malloc(size_t) __ATTR_MALLOC__;
-extern void free(void *);
+   In a C++ context, global destructors will be called before halting
+   execution.
+*/
+extern void exit(int __status) __ATTR_NORETURN__;
 
-/*
- * When extending the data segment, the allocator will not try to go
- * beyond the current stack limit, decreased by __malloc_margin bytes.
+/**
+   The malloc() function allocates \c size bytes of memory.
+   If malloc() fails, a NULL pointer is returned.
+
+   Note that malloc() does \e not initialize the returned memory to
+   zero bytes.
+*/
+extern void *malloc(size_t __size) __ATTR_MALLOC__;
+
+/**
+   The free() function causes the allocated memory referenced by \c
+   ptr to be made available for future allocations.  If \c ptr is
+   NULL, no action occurs.
+*/
+extern void free(void *__ptr);
+
+/**
+ * When extending the data segment in malloc(), the allocator will not try to go
+ * beyond the current stack limit, decreased by \c __malloc_margin bytes.
  * Thus, all possible stack frames of interrupt routines that could
  * interrupt the current function, plus all further nested function
  * calls must not require more stack space, or they'll risk to collide
  * with the data segment.
  *
- * The default is set to 32.  __malloc_margin should be changed before
+ * The default is set to 32.  \c __malloc_margin should be changed before
  * the very first call to malloc() within the application.
+ *
+ * All this is only relevant in situations where the heap is allocated
+ * below the stack.  For devices with external memory, the heap can
+ * be located in external memory while the stack is usually located in
+ * internal SRAM, so no special guard area is needed between both.
  */
 extern size_t __malloc_margin;
 
+/** See \c __malloc_heap_end. */
 extern char *__malloc_heap_start;
+/**
+   The variables \c __malloc_heap_start and \c __malloc_heap_end can
+   be used to restrict the malloc() function to a certain memory
+   region.  These variables are statically initialized to point to
+   \c __heap_start and \c __heap_end, respectively, where
+   \c __heap_start is filled in by the linker, and \c __heap_end is
+   set to 0 which makes malloc() assume the heap is below the stack.
+   Any changes need to be made before the very first call to malloc().
+
+   In case of a device with external SRAM where the heap is going to
+   be allocated in external RAM, it's good practice to already define
+   those symbols from the linker command line.
+*/
 extern char *__malloc_heap_end;
 
 /* implemented but not tested */
-extern double strtod(const char *, char **);
+/**
+     The strtod() function converts the initial portion of the string pointed
+     to by \c nptr to double representation.
 
-/* non-standard (i.e. non-ANSI C) functions */
+     The expected form of the string is an optional plus (``+'') or minus sign
+     (``-'') followed by a sequence of digits optionally containing a decimal-
+     point character, optionally followed by an exponent.  An exponent con­
+     sists of an ``E'' or ``e'', followed by an optional plus or minus sign,
+     followed by a sequence of digits.
+
+     Leading white-space characters in the string
+     are skipped.
+
+     The strtod() function returns the converted value, if any.
+
+     If \c endptr is not \c NULL, a pointer to the character after the last
+     character used in the conversion is stored in the location referenced by
+     \c endptr.
+
+     If no conversion is performed, zero is returned and the value of \c nptr is
+     stored in the location referenced by \c endptr.
+
+     If the correct value would cause overflow, plus or minus \c HUGE_VAL is
+     returned (according to the sign of the value), and \c ERANGE is stored in
+     \c errno.  If the correct value would cause underflow, zero is returned and
+     \c ERANGE is stored in \c errno.
+
+     FIXME: HUGE_VAL needs to be defined somewhere.  The bit pattern is
+     0x7fffffff, but what number would this be?
+ */
+
+extern double strtod(const char *__nptr, char **__endptr);
+/*@}*/
+
+/*@{*/
+/** \name Non-standard (i.e. non-ISO C) functions */
+/**
+   The function itoa() converts the integer value from \c val into an
+   ASCII representation that will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done using the \c radix as base, which may be a
+   number between 2 (binary conversion) and up to 36.  If \c radix
+   is greater than 10, the next digit after ``9'' will be the letter
+   ``a''.
+
+   The itoa() function returns the pointer passed as \c s.
+*/
 extern char *itoa(int __val, char *__s, int __radix);
+
+/**
+   The function ltoa() converts the long integer value from \c val into an
+   ASCII representation that will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done using the \c radix as base, which may be a
+   number between 2 (binary conversion) and up to 36.  If \c radix
+   is greater than 10, the next digit after ``9'' will be the letter
+   ``a''.
+
+   The ltoa() function returns the pointer passed as \c s.
+*/
 extern char *ltoa(long int __val, char *__s, int __radix);
+
+/**
+   The function utoa() converts the unsigned integer value from \c val into an
+   ASCII representation that will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done using the \c radix as base, which may be a
+   number between 2 (binary conversion) and up to 36.  If \c radix
+   is greater than 10, the next digit after ``9'' will be the letter
+   ``a''.
+
+   The utoa() function returns the pointer passed as \c s.
+*/
 extern char *utoa(unsigned int __val, char *__s, int __radix);
+
+/**
+   The function ultoa() converts the unsigned long integer value from
+   \c val into an
+   ASCII representation that will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done using the \c radix as base, which may be a
+   number between 2 (binary conversion) and up to 36.  If \c radix
+   is greater than 10, the next digit after ``9'' will be the letter
+   ``a''.
+
+   The i
+   ultoa() function returns the pointer passed as \c s.
+*/
 extern char *ultoa(unsigned long int __val, char *__s, int __radix);
-extern char *dtostre(double __val, char *__s, unsigned char __ndig, unsigned char __flags);
+
+#endif /* __ASSEMBLER */
+/*@}*/
+
+/*@{*/
+/** \name Conversion functions for double arguments. */
+/** Bit value that can be passed in \c flags to dtostre(). */
+#define DTOSTRE_ALWAYS_SIGN 0x01        /* put '+' or ' ' for positives */
+/** Bit value that can be passed in \c flags to dtostre(). */
+#define DTOSTRE_PLUS_SIGN   0x02        /* put '+' rather than ' ' */
+/** Bit value that can be passed in \c flags to dtostre(). */
+#define DTOSTRE_UPPERCASE   0x04        /* put 'E' rather 'e' */
+
+#ifndef __ASSEMBLER__
+
+/**
+   The dtostre() function converts the double value passed in \c val into
+   an ASCII representationthat will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done into in the style [-]d.ddde±dd where there is
+   one digit before the decimal-point character and the number of
+   digits after it is equal to the precision \c prec; if the precision
+   is zero, no decimal-point character appears.  If \c flags has the
+   DTOSTRE_UPPERCASE bit set, the letter `E' (rather than `e') will be
+   used to introduce the exponent.  The exponent always contains two
+   digits; if the value is zero, the exponent is 00.
+
+   If \c flags has the DTOSTRE_ALWAYS_SIGN bit set, a space character
+   will be placed into the leading position for positive numbers.
+
+   If \c flags has the DTOSTRE_PLUS_SIGN bit set, a plus sign will be
+   used instead of a space character in this case.
+*/
+extern char *dtostre(double __val, char *__s, unsigned char __prec,
+		     unsigned char __flags);
+
+/**
+   The dtostrf() function converts the double value passed in \c val into
+   an ASCII representationthat will be stored under \c s.  The caller
+   is responsible for providing sufficient storage in \c s.
+
+   Conversion is done into in the style [-]d.ddd.  The minimum field
+   width of the output string (including the ``.'' and the possible
+   sign for negative values) is given in \c width, and \c prec determines
+   the number of digits after the decimal sign.
+*/
 extern char *dtostrf(double __val, char __width, char __prec, char *__s);
+
+/*@}*/
 
 #if 0  /* not yet implemented */
 extern int atexit(void (*)(void));
@@ -165,5 +495,7 @@ extern void srand(unsigned int);
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* __ASSEMBLER */
 
 #endif /* _STDLIB_H_ */
