df04d19cf3a9079ffb3ee16d92ac1c13eddc18a8
Eric B. Weddington <eric.weddington@atmel.com>
2004-10-29 21:19:22 +0000

    * include/avr/io43u32x.h: Correct license with permission from Keith Gudger. 	* include/avr/io43u35x.h: Ditto. 	* include/avr/io86r401.h: Correct license with permission from Colin 	O'Flynn. 	* include/avr/iocan128.h: Ditto.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@595 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/iocan128.h
+++ include/avr/iocan128.h
@@ -1,14 +1,20 @@
 /* Copyright (c) 2004, Colin O'Flynn <coflynn@newae.com>
+   All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
 
    * Redistributions of source code must retain the above copyright
      notice, this list of conditions and the following disclaimer.
+
    * Redistributions in binary form must reproduce the above copyright
      notice, this list of conditions and the following disclaimer in
      the documentation and/or other materials provided with the
      distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
