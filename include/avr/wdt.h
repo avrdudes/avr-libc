e3a3840cae2862f22ff631b9aaed01ddb1edd7db
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2005-11-06 21:30:13 +0000

    (Patch submitted by Colin O'Flynn.) Protect wdt_disable against interrupts, resolves bug #14327 overview: wdt_disable() missing a cli
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/branches/avr-libc-1_2-branch/avr-libc@941 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/wdt.h
+++ include/avr/wdt.h
@@ -137,6 +137,7 @@
 #define wdt_disable() \
 __asm__ __volatile__ (  \
     "in __tmp_reg__, __SREG__" "\n\t" \
+    "cli" "\n\t" \
     "sts %0, %1" "\n\t" \
     "sts %0, __zero_reg__" "\n\t" \
     "out __SREG__,__tmp_reg__" "\n\t" \
@@ -175,6 +176,7 @@ __asm__ __volatile__ (  \
 #define wdt_disable() \
 __asm__ __volatile__ (  \
     "in __tmp_reg__, __SREG__" "\n\t" \
+     "cli" "\n\t" \
     "out %0, %1" "\n\t" \
     "out %0, __zero_reg__" "\n\t" \
     "out __SREG__,__tmp_reg__" "\n\t" \
