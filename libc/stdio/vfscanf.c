46ac11bd0843c69d47d5ca23f7f113fd2d3d8964
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2005-11-09 22:01:25 +0000

    Merge from HEAD:
    Fix the return value for starred format specifiers, and return the
    number of assignments rather than the number of conversions.
    
    Fixes bug #14104: sscanf returns number of matches instead of
    number of assignments
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/branches/avr-libc-1_2-branch/avr-libc@946 1084037a-409f-4a99-8160-1a4b3577d4c1

--- libc/stdio/vfscanf.c
+++ libc/stdio/vfscanf.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2004 Joerg Wunsch
+/* Copyright (c) 2002,2004,2005 Joerg Wunsch
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
@@ -104,7 +104,7 @@ vfscanf(FILE *stream, const char *fmt, va_list ap) {
 	} a;
 	char	c;	/* holds a char from the format string */
 	uint8_t	base;
-	int	nconvs, i, j, olen;
+	int	nconvs, rv, i, j, olen;
 #if SCANF_LEVEL > SCANF_MIN
 	int8_t	width;
 #endif
@@ -117,6 +117,7 @@ vfscanf(FILE *stream, const char *fmt, va_list ap) {
 	flags = 0;
 	nconvs = 0;
 	i = 0;
+	rv = EOF;
 	olen = stream->len = 0;
 #if SCANF_LEVEL >= SCANF_FLT
 	buf = 0;
@@ -545,15 +546,19 @@ vfscanf(FILE *stream, const char *fmt, va_list ap) {
 #if SCANF_LEVEL >= SCANF_FLT
 		  nextconv:
 #endif
-			flags = 0;
-			if (stream->len > olen)
-				nconvs++;
-			else if (c != 'n' || i == EOF)
+			if (stream->len > olen) {
+#if SCANF_LEVEL >= SCANF_STD
+				if (!(flags & FLSTAR))
+#endif
+					nconvs++;
+				rv = 0;
+			} else if (c != 'n' || i == EOF)
 				/*
 				 * If one conversion failed completely,
 				 * punt.
 				 */
 				goto leave;
+			flags = 0;
 		} else if (c == '%') {
 			flags = FLHASPERCENT;
 			base = 10;
@@ -584,19 +589,25 @@ leave:
 	 * or end-of-file), adjust the total number of conversions
 	 * done if at least one char could be read from the stream.
 	 */
-	if ((flags & FLHASPERCENT) && stream->len > olen)
-		nconvs++;
+	if ((flags & FLHASPERCENT) && stream->len > olen) {
+#if SCANF_LEVEL >= SCANF_STD
+		if (!(flags & FLSTAR))
+#endif
+			nconvs++;
+		rv = 0;
+	}
+
+#if SCANF_LEVEL >= SCANF_FLT
+	free(buf);
+#endif
 	/*
 	 * If an error occurs before the first successful conversion,
 	 * we ought to return EOF.  Before getting here, all
 	 * conversions maintain the last character read from the
 	 * stream (or EOF) within variable `i'.
 	 */
-	if (nconvs == 0 && i == EOF)
-		nconvs = EOF;
+	if (i == EOF && nconvs == 0)
+		return rv;
 
-#if SCANF_LEVEL >= SCANF_FLT
-	free(buf);
-#endif
 	return nconvs;
 }
