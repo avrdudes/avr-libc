00c60d9b503ed368ce767bfb9cbf3082ba48b2e3
Eric B. Weddington <eric.weddington@atmel.com>
2007-10-03 22:43:39 +0000

    2007-10-03  Eric B. Weddington  <eweddington@cso.atmel.com>
    	Add new device: ATtiny43U.
    	* configure.ac: Add new device.
    	* devtools/gen-avr-lib-tree.sh: ditto.
    	* include/avr/Makefile.am: ditto.
    	* include/avr/io.h: ditto.
    	* include/avr/power.h: ditto.
    	* doc/api/using-tools.dox: ditto.
    	* doc/api/main_page.dox: ditto.
    	* include/avr/iotn43u.h: New file.
    	* NEWS: Update new devices list.
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/branches/avr-libc-1_4-branch/avr-libc@1427 1084037a-409f-4a99-8160-1a4b3577d4c1

--- devtools/gen-avr-lib-tree.sh
+++ devtools/gen-avr-lib-tree.sh
@@ -168,7 +168,8 @@ attiny45:crttn45.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS};\
 attiny461:crttn461.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS};\
 attiny84:crttn84.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS};\
 attiny85:crttn85.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS};\
-attiny861:crttn861.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS}\
+attiny861:crttn861.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS};\
+attiny43u:crttn43u.o:${DEV_DEFS}:${CFLAGS_SPACE}:${DEV_ASFLAGS}\
 "
 
 AVR3_DEV_INFO="\
