7998977d0de3f2f0b676280c17136fe5d544e735
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2007-10-29 14:01:18 +0000

    Submitted by Dean Camera: Patch #5644: New interrupt.h header file * include/avr/interrupt.h: Complete overhaul of the ISR() and related macros. By Joerg Wunsch: * doc/api/interrupts.dox: Update the interrupt documentation to all the new features.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@1455 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/interrupt.h
+++ include/avr/interrupt.h
@@ -1,4 +1,6 @@
 /* Copyright (c) 2002,2005,2007 Marek Michalkiewicz
+   Copyright (c) 2007, Dean Camera
+
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
@@ -34,6 +36,11 @@
 #define _AVR_INTERRUPT_H_
 
 #include <avr/io.h>
+
+#if !defined(__DOXYGEN__) && !defined(__STRINGIFY)
+/* Auxiliary macro for ISR_ALIAS(). */
+#define __STRINGIFY(x) #x
+#endif /* !defined(__DOXYGEN__) */
 
 /** 
 \file 
@@ -80,18 +87,27 @@
 
 
 #if defined(__DOXYGEN__)
-/** \def ISR(vector)
+/** \def ISR(vector [, attributes])
     \ingroup avr_interrupts
 
     \code #include <avr/interrupt.h> \endcode
 
     Introduces an interrupt handler function (interrupt service
-    routine) that runs with global interrupts initially disabled.
+    routine) that runs with global interrupts initially disabled
+    by default with no attributes specified.
+
+    The attributes are optional and alter the behaviour and resultant
+    generated code of the interrupt routine. Multiple attributes may
+    be used for a single function, with a space seperating each
+    attribute.
+
+    Valid attributes are ISR_BLOCK, ISR_NOBLOCK, ISR_NAKED and
+    ISR_ALIASOF(vect).
 
     \c vector must be one of the interrupt vector names that are
     valid for the particular MCU type.
 */
-#  define ISR(vector)
+#  define ISR(vector, [attributes])
 #else  /* real code */
 
 #if __GNUC__ >= 4 && __GNUC_MINOR__ >= 1
@@ -101,13 +117,13 @@
 #endif
 
 #ifdef __cplusplus
-#define ISR(vector)					\
-extern "C" void vector(void) __attribute__ ((signal,__INTR_ATTRS));	\
-void vector (void)
+#  define ISR(vector, ...)            \
+    extern "C" void vector (void) __attribute__ ((signal,__INTR_ATTRS)) __VA_ARGS__; \
+    void vector (void)
 #else
-#define ISR(vector)					\
-void vector (void) __attribute__ ((signal,__INTR_ATTRS));		\
-void vector (void)
+#  define ISR(vector, ...)            \
+    void vector (void) __attribute__ ((signal,__INTR_ATTRS)) __VA_ARGS__; \
+    void vector (void)
 #endif
 
 #endif /* DOXYGEN */
@@ -121,20 +137,20 @@ void vector (void)
     Introduces an interrupt handler function that runs with global interrupts
     initially disabled.
 
-    This is the same as the ISR macro.
+    This is the same as the ISR macro without optional attributes.
     \deprecated Do not use SIGNAL() in new code. Use ISR() instead.
 */
 #  define SIGNAL(vector)
 #else  /* real code */
 
 #ifdef __cplusplus
-#define SIGNAL(vector)					\
-extern "C" void vector(void) __attribute__ ((signal,__INTR_ATTRS));	\
-void vector (void)
+#  define SIGNAL(vector)					\
+    extern "C" void vector(void) __attribute__ ((signal, __INTR_ATTRS));	\
+    void vector (void)
 #else
-#define SIGNAL(vector)					\
-void vector (void) __attribute__ ((signal,__INTR_ATTRS));		\
-void vector (void)
+#  define SIGNAL(vector)					\
+    void vector (void) __attribute__ ((signal, __INTR_ATTRS));		\
+    void vector (void)
 #endif
 
 #endif /* DOXYGEN */
@@ -150,25 +166,20 @@ void vector (void)
     define a function body as this will define it for you.
     Example:
     \code EMPTY_INTERRUPT(ADC_vect);\endcode */
-#define EMPTY_INTERRUPT(vector)
+#  define EMPTY_INTERRUPT(vector)
 #else  /* real code */
 
 #ifdef __cplusplus
-#define EMPTY_INTERRUPT(vector)                \
-extern "C" void vector(void) __attribute__ ((signal,naked,__INTR_ATTRS));    \
-void vector (void) {  __asm__ __volatile__ ("reti" ::); }
+#  define EMPTY_INTERRUPT(vector)                \
+    extern "C" void vector(void) __attribute__ ((signal,naked,__INTR_ATTRS));    \
+    void vector (void) {  __asm__ __volatile__ ("reti" ::); }
 #else
-#define EMPTY_INTERRUPT(vector)                \
-void vector (void) __attribute__ ((signal,naked,__INTR_ATTRS));    \
-void vector (void) { __asm__ __volatile__ ("reti" ::); }
+#  define EMPTY_INTERRUPT(vector)                \
+    void vector (void) __attribute__ ((signal,naked,__INTR_ATTRS));    \
+    void vector (void) { __asm__ __volatile__ ("reti" ::); }
 #endif
 
 #endif /* DOXYGEN */
-
-#if !defined(__DOXYGEN__)
-/* Auxiliary macro for ISR_ALIAS(). */
-#define __ALIAS(tgt) __attribute__((alias(#tgt)))
-#endif /* !defined(__DOXYGEN__) */
 
 #if defined(__DOXYGEN__)
 /** \def ISR_ALIAS(vector, target_vector)
@@ -176,14 +187,19 @@ void vector (void) { __asm__ __volatile__ ("reti" ::); }
 
     \code #include <avr/interrupt.h> \endcode
 
-    Defines \c vector to point to the same interrupt vector as
-    \c target_vector.  That way, a single interrupt vector
-    implementation can be used to serve several interrupt sources.
+    Aliases a given vector to another one in the same manner as the
+    ISR_ALIASOF attribute for the ISR() macro. Unlike the ISR_ALIASOF
+    attribute macro however, this is compatible for all versions of
+    GCC rather than just GCC version 4.2 onwards.
 
-    Do not define a function body.
+    \note This macro creates a trampoline function for the aliased
+    macro.  This will result in a two cycle penalty for the aliased
+    vector compared to the ISR the vector is aliased to, due to the
+    JMP/RJMP opcode used.
 
-    \note This requires a recent version of AVR-GCC to work
-    (GCC 4.2 or patched GCC 4.1.x).
+    \deprecated
+    For new code, the use of ISR(..., ISR_ALIASOF(...))  is
+    recommended.
 
     Example:
     \code
@@ -195,17 +211,133 @@ void vector (void) { __asm__ __volatile__ ("reti" ::); }
     ISR_ALIAS(INT1_vect, INT0_vect);
     \endcode 
 */
-#define ISR_ALIAS(vector, target_vector)
+#  define ISR_ALIAS(vector, target_vector)
 #else /* real code */
 
 #ifdef __cplusplus
-#define ISR_ALIAS(vector, tgt) extern "C" void vector(void) __ALIAS(tgt)
-#else
-#define ISR_ALIAS(vector, tgt) void vector(void) __ALIAS(tgt)
-#endif
+#  if defined(__AVR_MEGA__) && __AVR_MEGA__
+#    define ISR_ALIAS(vector, tgt) extern "C" void vector (void) \
+	__attribute__((signal, naked, __INTR_ATTRS)); \
+	void vector (void) { asm volatile ("jmp " __STRINGIFY(tgt) ::); }
+#  else /* !__AVR_MEGA */
+#    define ISR_ALIAS(vector, tgt) extern "C" void vector (void) \
+	__attribute__((signal, naked, __INTR_ATTRS)); \
+	void vector (void) { asm volatile ("rjmp " __STRINGIFY(tgt) ::); }
+#  endif  /* __AVR_MEGA__ */
+#else	  /* !__cplusplus */
+#  if defined(__AVR_MEGA__) && __AVR_MEGA__
+#  define ISR_ALIAS(vector, tgt) void vector (void) \
+	__attribute__((signal, naked, __INTR_ATTRS)); \
+	void vector (void) { asm volatile ("jmp " __STRINGIFY(tgt) ::); }
+#  else /* !__AVR_MEGA */
+#  define ISR_ALIAS(vector, tgt) void vector (void) \
+	__attribute__((signal, naked, __INTR_ATTRS)); \
+	void vector (void) { asm volatile ("rjmp " __STRINGIFY(tgt) ::); }
+#  endif  /* __AVR_MEGA__ */
+#endif	/* __cplusplus */
 
 #endif /* DOXYGEN */
 
+#if defined(__DOXYGEN__)
+/** \def reti()
+    \ingroup avr_interrupts
+
+    \code #include <avr/interrupt.h> \endcode
+
+    Returns from an interrupt routine, enabling global interrupts. This should
+    be the last command executed before leaving an ISR defined with the ISR_NAKED
+    attribute.
+
+    This macro actually compiles into a single line of assembly, so there is
+    no function call overhead.
+*/
+#  define reti()
+#else  /* !DOXYGEN */
+#  define reti()  __asm__ __volatile__ ("reti" ::)
+#endif /* DOXYGEN */
+
+#if defined(__DOXYGEN__)
+/** \def BADISR_vect
+    \ingroup avr_interrupts
+
+    \code #include <avr/interrupt.h> \endcode
+
+    This is a vector which is aliased to __vector_default, the vector
+    executed when an ISR fires with no accompanying ISR handler. This
+    may be used along with the ISR() macro to create a catch-all for
+    undefined but used ISRs for debugging purposes.
+*/
+#  define BADISR_vect
+#else  /* !DOXYGEN */
+#  define BADISR_vect __vector_default
+#endif /* DOXYGEN */
+
+/** \name ISR attributes */
+
+#if defined(__DOXYGEN__)
+/** \def ISR_BLOCK
+    \ingroup avr_interrupts
+
+    \code# include <avr/interrupt.h> \endcode
+
+    Identical to an ISR with no attributes specified. Global
+    interrupts are initially disabled by the AVR hardware when
+    entering the ISR, without the compiler modifying this state.
+
+    Use this attribute in the attributes parameter of the ISR macro.
+*/
+#  define ISR_BLOCK
+
+/** \def ISR_NOBLOCK
+    \ingroup avr_interrupts
+
+    \code# include <avr/interrupt.h> \endcode
+
+    ISR runs with global interrupts initially enabled.  The interrupt
+    enable flag is activated by the compiler as early as possible
+    within the ISR to ensure minimal processing delay for nested
+    interrupts.
+
+    This may be used to create nested ISRs, however care should be
+    taken to avoid stack overflows, or to avoid infinitely entering
+    the ISR for those cases where the AVR hardware does not clear the
+    respective interrupt flag before entering the ISR.
+
+    Use this attribute in the attributes parameter of the ISR macro.
+*/
+#  define ISR_NOBLOCK
+
+/** \def ISR_NAKED
+    \ingroup avr_interrupts
+
+    \code# include <avr/interrupt.h> \endcode
+
+    ISR is created with no prologue or epilogue code. The user code is
+    responsible for preservation of the machine state including the
+    SREG register, as well as placing a reti() at the end of the
+    interrupt routine.
+
+    Use this attribute in the attributes parameter of the ISR macro.
+*/
+#  define ISR_NAKED
+
+/** \def ISR_ALIASOF(target_vector)
+    \ingroup avr_interrupts
+
+    \code#include <avr/interrupt.h>\endcode
+
+    The ISR is linked to another ISR, specified by the vect parameter.
+    This is compatible with GCC 4.2 and greater only.
+
+    Use this attribute in the attributes parameter of the ISR macro.
+*/
+#  define ISR_ALIASOF(target_vector)
+#else  /* !DOXYGEN */
+#  define ISR_BLOCK
+#  define ISR_NOBLOCK    __attribute__((interrupt))
+#  define ISR_NAKED      __attribute__((naked))
+#  define ISR_ALIASOF(v) __attribute__((alias(__STRINGIFY(v))))
+#endif /* DOXYGEN */
 
 /* \@} */
 
