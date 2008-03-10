9ffb6fca0ede803b467c2191d226b0f86b46b5d0
Eric B. Weddington <eric.weddington@atmel.com>
2008-03-10 06:42:04 +0000

    2008-03-10  Eric B. Weddington  <eric.weddington@atmel.com>
    	* configure.ac: Add support for ATmega32M1 device.
    	* devtools/gen-avr-lib-tree.sh: Ditto.
    	* doc/api/main_page.dox: Ditto.
    	* doc/api/using-tools.dox: Ditto.
    	* include/avr/Makefile.am: Ditto.
    	* include/avr/io.h: Ditto.
    	* include/avr/power.h: Ditto.
    	* include/avr/wdt.h: Ditto.
    	* include/avr/iom32m1.h: New file.
    	* NEWS: Add new device to list.
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/branches/avr-libc-1_6-branch/avr-libc@1617 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/io.h
+++ include/avr/io.h
@@ -117,6 +117,8 @@
 #  include <avr/io90pwm216.h>
 #elif defined (__AVR_AT90PWM316__)
 #  include <avr/io90pwm316.h>
+#elif defined (__AVR_ATmega32M1__)
+#  include <avr/iom32m1.h>
 #elif defined (__AVR_ATmega128__)
 #  include <avr/iom128.h>
 #elif defined (__AVR_ATmega1280__)
