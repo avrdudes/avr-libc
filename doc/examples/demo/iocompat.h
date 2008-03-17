4a3e41be86fe67b742c7026ac3a73df037249c8e
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2008-03-17 21:52:00 +0000

    Add support for ATmega2560 and ATmega2561 as well.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@1638 1084037a-409f-4a99-8160-1a4b3577d4c1

--- doc/examples/demo/iocompat.h
+++ doc/examples/demo/iocompat.h
@@ -120,7 +120,8 @@
       defined(__AVR_ATmega329__) || defined(__AVR_ATmega3290__) || \
       defined(__AVR_ATmega649__) || defined(__AVR_ATmega6490__) || \
       defined(__AVR_ATmega640__) || \
-      defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__)
+      defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || \
+      defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)
 #  define OC1 PB5
 #  define DDROC DDRB
 #  define OCR OCR1A
