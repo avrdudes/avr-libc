d400d8ef8a6a60c08487dd6785c889c869e38975
dmix Dmitry Xmelkov <dmix@gmail.ru>
2008-12-13 04:26:46 +0000

    Fix for bug #25048: eeprom.h will not compile as c++ code. New file. TODO: add to 'runtest.sh' the possibility to operate C++ files. Today this file is ignored.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/branches/avr-libc-1_6-branch/avr-libc@1822 1084037a-409f-4a99-8160-1a4b3577d4c1

---
+++ tests/simulate/regression/bug-25048.cpp
@@ -0,0 +1,19 @@
+/* Avr-libc bug #25048: eeprom.h will not compile as c++ code.
+   This is compile-time bug. It is actual for chips with small amount
+   EEPROM memory, i.e. AT90S2313.
+   $Id$	*/
+
+#ifndef	__AVR__
+int main ()	{ return 0; }
+#else
+
+#include <avr/eeprom.h>
+
+EEMEM unsigned char c;
+
+int main ()
+{
+    eeprom_write_byte (&c, 0);
+    return eeprom_read_byte (&c);
+}
+#endif
