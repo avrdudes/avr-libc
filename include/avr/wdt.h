9edb89ead08c4809039b1cadeea473be9464fcdd
Eric B. Weddington <eric.weddington@atmel.com>
2009-02-13 22:19:48 +0000

    2009-02-13  Eric B. Weddington  <eric.weddington@atmel.com>
    	* configure.ac: Add support for ATxmega32D4, ATxmega16A4, ATxmega16D4.
    	* devtools/gen-avr-lib-tree.sh: Same.
    	* include/Makefile.am: Same.
    	* include/io.h: Same.
    	* doc/api/main_page.dox: Same.
    	* doc/api/using-tools.dox: Same.
    	* include/avr/power.h: Same.
    	* include/avr/wdt.h: Same.
    	* NEWS: Same.
    	* devtools/gen-avr-lib-tree.sh (CFLAGS_SPACE_NO_CALL): Add. Optimize for
    	space but without -mcall-prologues flag.
    	(AVR6_DEV_INFO) Use CFLAGS_SPACE_NO_CALL.
    	(AVRXMEGA6_DEV_INFO) Use CFLAGS_SPACE_NO_CALL on devices with 256K+ flash.
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@1881 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/wdt.h
+++ include/avr/wdt.h
@@ -130,7 +130,10 @@
 */
 
 
-#if defined(__AVR_ATxmega32A4__) \ 
+#if defined(__AVR_ATxmega16A4__) \
+|| defined(__AVR_ATxmega16D4__) \
+|| defined(__AVR_ATxmega32A4__) \
+|| defined(__AVR_ATxmega32D4__) \
 || defined(__AVR_ATxmega64A1__) \
 || defined(__AVR_ATxmega64A3__) \
 || defined(__AVR_ATxmega128A1__) \
