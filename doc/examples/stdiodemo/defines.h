b4d5ef376f6d44d8a6f273cc74f6b21384343525
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2009-06-25 20:21:16 +0000

    Allow the HD44780 connection in stdiodemo to be spread across more than a single port. * doc/examples/stdiodemo/defines.h: Pin assignments now contain both, a port name as well as a pin number. * doc/examples/stdiodemo/hd44780.c: Implement the new macros to access the port pins. * doc/examples/stdiodemo/hd44780.h: Mention the newly added function hd44780_powerdown() that prepares the port pins to safely power down the HD44780 (externally). * doc/examples/stdiodemo/stdiodemo.dox: Adapt to the changes implemented.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2002 1084037a-409f-4a99-8160-1a4b3577d4c1

--- doc/examples/stdiodemo/defines.h
+++ doc/examples/stdiodemo/defines.h
@@ -18,12 +18,12 @@
 #define UART_BAUD  9600
 
 /* HD44780 LCD port connections */
-#define HD44780_PORT A
-#define HD44780_RS PORT6
-#define HD44780_RW PORT4
-#define HD44780_E  PORT5
+#define HD44780_RS A, 6
+#define HD44780_RW A, 4
+#define HD44780_E  A, 5
 /* The data bits have to be in ascending order. */
-#define HD44780_D4 PORT0
-#define HD44780_D5 PORT1
-#define HD44780_D6 PORT2
-#define HD44780_D7 PORT3
+#define HD44780_D4 A, 0
+
+/* Whether to read the busy flag, or fall back to
+   worst-time delays. */
+#define USE_BUSY_BIT 1
