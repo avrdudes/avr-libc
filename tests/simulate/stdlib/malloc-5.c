28d109f8ada6448abdbb672d425b2fe4676de28e
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2010-06-07 16:27:16 +0000

    More testscripts: test free() works as expected.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2130 1084037a-409f-4a99-8160-1a4b3577d4c1

---
+++ tests/simulate/stdlib/malloc-5.c
@@ -0,0 +1,78 @@
+/* Copyright (c) 2010  Joerg Wunsch
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id$ */
+
+/* Freelist test #1: bottom of freelist. */
+
+#ifndef	__AVR__
+
+/* There is no sense to check on host computer. */
+int main ()
+{
+    return 0;
+}
+
+#else
+
+#include <stdint.h>
+#include <stdlib.h>
+
+struct __freelist {
+        size_t sz;
+        struct __freelist *nx;
+};
+
+extern char *__brkval;          /* first location not yet allocated */
+extern struct __freelist *__flp; /* freelist pointer (head of freelist) */
+extern char *__malloc_heap_start;
+extern char *__malloc_heap_end;
+
+int main(void)
+{
+    void *ptrs[6];
+    uint8_t i;
+
+    for (i = 0; i < 6; i++)
+    {
+        void *p = malloc(10);
+        if (p == NULL) return __LINE__;
+        ptrs[i] = p;
+    }
+
+    free(ptrs[0]);
+    /* There must be exactly one entry in freelist. */
+    if ((char *)(ptrs[0]) - 2 != (void *)__flp) return __LINE__;
+    if (__flp->sz != 10) return __LINE__;
+    if (__flp->nx != NULL) return __LINE__;
+
+    return 0;
+}
+
+#endif  /* !AVR */
---
+++ tests/simulate/stdlib/malloc-7.c
@@ -0,0 +1,107 @@
+/* Copyright (c) 2010  Joerg Wunsch
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* $Id$ */
+
+/* Freelist test #3: aggregation test. */
+
+#ifndef	__AVR__
+
+/* There is no sense to check on host computer. */
+int main ()
+{
+    return 0;
+}
+
+#else
+
+#include <stdint.h>
+#include <stdlib.h>
+
+struct __freelist {
+        size_t sz;
+        struct __freelist *nx;
+};
+
+extern char *__brkval;          /* first location not yet allocated */
+extern struct __freelist *__flp; /* freelist pointer (head of freelist) */
+extern char *__malloc_heap_start;
+extern char *__malloc_heap_end;
+
+int main(void)
+{
+    void *ptrs[6];
+    uint8_t i;
+
+    for (i = 0; i < 6; i++)
+    {
+        void *p = malloc(10);
+        if (p == NULL) return __LINE__;
+        ptrs[i] = p;
+    }
+
+    free(ptrs[5]);
+    /* There must be exactly one entry in freelist. */
+    if ((char *)(ptrs[5]) - 2 != (void *)__flp) return __LINE__;
+    if (__flp->sz != 10) return __LINE__;
+    if (__flp->nx != NULL) return __LINE__;
+    if ((char *)(ptrs[5]) + 10 != (void *)__brkval) return __LINE__;
+
+    free(ptrs[4]);
+    /* Still one entry expected. */
+    if ((char *)(ptrs[4]) - 2 != (void *)__flp) return __LINE__;
+    if (__flp->sz != 22) return __LINE__;
+    if (__flp->nx != NULL) return __LINE__;
+    if ((char *)(ptrs[4]) + 22 != (void *)__brkval) return __LINE__;
+
+    struct __freelist *ofp = __flp;
+
+    free(ptrs[1]);
+    /* Another entry added. */
+    if ((char *)(ptrs[1]) - 2 != (void *)__flp) return __LINE__;
+    if (__flp->sz != 10) return __LINE__;
+    if (__flp->nx != (void *)ofp) return __LINE__;
+
+    free(ptrs[3]);
+    /* Second entry must have been extended. */
+    if ((char *)(ptrs[3]) - 2 != (void *)__flp->nx) return __LINE__;
+    if (__flp->nx->sz != 34) return __LINE__;
+    if (__flp->nx->nx != NULL) return __LINE__;
+    if ((char *)(ptrs[3]) + 34 != (void *)__brkval) return __LINE__;
+
+    free(ptrs[2]);
+    /* Both chunks must have been combined. */
+    if (__flp->sz != 58) return __LINE__;
+    if (__flp->nx != NULL) return __LINE__;
+    if ((char *)(ptrs[1]) + 58 != (void *)__brkval) return __LINE__;
+
+    return 0;
+}
+
+#endif  /* !AVR */
