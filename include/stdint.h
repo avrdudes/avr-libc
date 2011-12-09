2eab3d134d898a00bae5254768e574ffbb51cc86
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2011-12-09 07:05:46 +0000

    bug #35020: stdint.h: signed types need explicit "signed"
    * include/stdint.h: explicitly declare int*_t types as signed
    
    
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2261 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/stdint.h
+++ include/stdint.h
@@ -118,14 +118,14 @@ typedef unsigned long long int uint64_t;
 
 /* actual implementation goes here */
 
-typedef int int8_t __attribute__((__mode__(__QI__)));
+typedef signed int int8_t __attribute__((__mode__(__QI__)));
 typedef unsigned int uint8_t __attribute__((__mode__(__QI__)));
-typedef int int16_t __attribute__ ((__mode__ (__HI__)));
+typedef signed int int16_t __attribute__ ((__mode__ (__HI__)));
 typedef unsigned int uint16_t __attribute__ ((__mode__ (__HI__)));
-typedef int int32_t __attribute__ ((__mode__ (__SI__)));
+typedef signed int int32_t __attribute__ ((__mode__ (__SI__)));
 typedef unsigned int uint32_t __attribute__ ((__mode__ (__SI__)));
 #if !__USING_MINT8
-typedef int int64_t __attribute__((__mode__(__DI__)));
+typedef signed int int64_t __attribute__((__mode__(__DI__)));
 typedef unsigned int uint64_t __attribute__((__mode__(__DI__)));
 #endif
 
