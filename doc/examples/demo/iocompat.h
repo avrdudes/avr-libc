b66fec6b70d73c29c52960dc55ac5a366189a7c4
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2013-05-03 12:38:46 +0000

    Submitted by W. Trevor King: patch #7826 Add ATMega32u4 support to the led-blinking demo
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2384 1084037a-409f-4a99-8160-1a4b3577d4c1

--- doc/examples/demo/iocompat.h
+++ doc/examples/demo/iocompat.h
@@ -121,7 +121,8 @@
       defined(__AVR_ATmega649__) || defined(__AVR_ATmega6490__) || \
       defined(__AVR_ATmega640__) || \
       defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || \
-      defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)
+      defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__) || \
+      defined(__AVR_ATmega32U4__)
 #  define OC1 PB5
 #  define DDROC DDRB
 #  define OCR OCR1A
