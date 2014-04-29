72eaf005f9b5be6e84aef3592a0bf360e10755cb
Pitchumani Sivanupandi <pitchumani.sivanupandi@microchip.com>
2014-04-29 10:33:23 +0000

    bug #39049: Add clock_prescale_set and get function/ macro for tiny 4/5/9/10/20 and 40 devices
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2422 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/power.h
+++ include/avr/power.h
@@ -2754,6 +2754,47 @@ clock_div_t clock_prescale_get(void)
     }
 }
  
+#elif defined(__AVR_ATtiny4__) \
+|| defined(__AVR_ATtiny5__) \
+|| defined(__AVR_ATtiny9__) \
+|| defined(__AVR_ATtiny10__) \
+|| defined(__AVR_ATtiny20__) \
+|| defined(__AVR_ATtiny40__) \
+
+typedef enum 
+{ 
+    clock_div_1 = 0, 
+    clock_div_2 = 1, 
+    clock_div_4 = 2, 
+    clock_div_8 = 3, 
+    clock_div_16 = 4, 
+    clock_div_32 = 5, 
+    clock_div_64 = 6, 
+    clock_div_128 = 7, 
+    clock_div_256 = 8 
+} clock_div_t; 
+
+static __inline__ void clock_prescale_set(clock_div_t) __attribute__((__always_inline__));
+
+void clock_prescale_set(clock_div_t __x)
+{
+    uint8_t __tmp = 0xD8;
+    __asm__ __volatile__ (
+        "in __tmp_reg__,__SREG__" "\n\t"
+        "cli" "\n\t"
+        "out %1, %0" "\n\t"
+        "out %2, %3" "\n\t"
+        "out __SREG__, __tmp_reg__"
+        : /* no outputs */
+        : "d" (__tmp),
+          "I" (_SFR_IO_ADDR(CCP)),
+          "I" (_SFR_IO_ADDR(CLKPSR)),
+          "d" (__x)
+        : "r16");
+}
+
+#define clock_prescale_get()  (clock_div_t)(CLKPSR & (uint8_t)((1<<CLKPS0)|(1<<CLKPS1)|(1<<CLKPS2)|(1<<CLKPS3)))
+ 
 #endif
 
 #endif /* _AVR_POWER_H_ */
