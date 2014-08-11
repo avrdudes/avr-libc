100e9444433efb605691060553cd7535901567fe
Joerg Wunsch <j.gnu@uriah.heep.sax.de>
2014-08-11 10:38:30 +0000

    Fix broken $Id$ line.
    git-svn-id: https://svn.savannah.nongnu.org/svn/avr-libc/trunk/avr-libc@2437 1084037a-409f-4a99-8160-1a4b3577d4c1

--- include/avr/iom2560.h
+++ include/avr/iom2560.h
@@ -28,7 +28,7 @@
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   POSSIBILITY OF SUCH DAMAGE. */
 
-/* $Id */
+/* $Id$ */
 
 /* avr/iom2560.h - definitions for ATmega2560 */
 
--- include/avr/iom2561.h
+++ include/avr/iom2561.h
@@ -28,7 +28,7 @@
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
   POSSIBILITY OF SUCH DAMAGE. */
 
-/* $Id */
+/* $Id$ */
 
 /* avr/iom2561.h - definitions for ATmega2561 */
 
