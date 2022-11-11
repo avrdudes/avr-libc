fe6e6197de7a2bd619eaaebf6b6a6bb81572b4ee
Georg-Johann Lay <gjl@gcc.gnu.org>
2022-11-11 17:21:05 +0100

    Extend _delay_loop_2 so it works with AVR_TINY.

--- include/util/delay_basic.h
+++ include/util/delay_basic.h
@@ -102,12 +102,20 @@ _delay_loop_1(uint8_t __count)
 void
 _delay_loop_2(uint16_t __count)
 {
+#if defined (__AVR_TINY__)
+	__asm__ volatile (
+		"1: subi %A0,1" "\n\t"
+		"   sbci %B0,0" "\n\t"
+		"brne 1b"
+		: "+d" (__count)
+	);
+#else
 	__asm__ volatile (
 		"1: sbiw %0,1" "\n\t"
 		"brne 1b"
-		: "=w" (__count)
-		: "0" (__count)
+		: "+w" (__count)
 	);
+#endif /* AVR_TINY */
 }
 
 #endif /* _UTIL_DELAY_BASIC_H_ */
