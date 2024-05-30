5ee73c4c9ea0f4f088dc88434aadb9ea510f30c2
Georg-Johann Lay <avr@gjlay.de>
2024-05-30 21:51:56 +0200

    math/all-float.c - Use tests that are less vulnerable to GCC bloat.

--- tests/simulate/math/all-float.c
+++ tests/simulate/math/all-float.c
@@ -33,116 +33,49 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define CMP(fun) if ((void*) fun != (void*) fun##f) exit (__LINE__)
+
 int main ()
 {
+#if defined(__AVR__) && __SIZEOF_DOUBLE__ == __SIZEOF_FLOAT__
     volatile float x = 0.25, y, z;
     volatile int i;
-    volatile long l;
-    double z1;
-    float z2;
-    int k1, k2;
 
-    y = cos (x);
-    z = cosf (x);
-    if (y != z) exit (__LINE__);
-
-    y = sin (x);
-    z = sinf (x);
-    if (y != z) exit (__LINE__);
-
-    y = tan (x);
-    z = tanf (x);
-    if (y != z) exit (__LINE__);
+    CMP (cos);
+    CMP (sin);
+    CMP (tan);
 
     y = fabs (x);
     z = fabsf (x);
     if (y != z) exit (__LINE__);
 
-    y = fmod (x, 0.75f);
-    z = fmodf (x, 0.75f);
-    if (y != z) exit (__LINE__);
+    CMP (fmod);
+    CMP (modf);
 
-    y = modf (x, &z1);
-    z = modff (x, &z2);
-    if (y != z || z1 != z2) exit (__LINE__);
+    CMP (sqrt);
+    CMP (cbrt);
+    CMP (hypot);
+    CMP (square);
 
-    y = sqrt (x);
-    z = sqrtf (x);
-    if (y != z) exit (__LINE__);
+    CMP (floor);
+    CMP (ceil);
 
-    y = cbrt (x);
-    z = cbrtf (x);
-    if (y != z) exit (__LINE__);
+    CMP (frexp);
+    CMP (ldexp);
 
-    y = hypot (x, 1.0f);
-    z = hypotf (x, 1.0f);
-    if (y != z) exit (__LINE__);
+    CMP (exp);
+    CMP (cosh);
+    CMP (sinh);
+    CMP (tanh);
 
-#ifdef	__AVR__
-    y = square (x);
-    z = squaref (x);
-    if (y != z) exit (__LINE__);
-#endif
+    CMP (acos);
+    CMP (asin);
+    CMP (atan);
+    CMP (atan2);
 
-    y = floor (x);
-    z = floorf (x);
-    if (y != z) exit (__LINE__);
-
-    y = ceil (x);
-    z = ceilf (x);
-    if (y != z) exit (__LINE__);
-
-    y = frexp (x, &k1);
-    z = frexpf (x, &k2);
-    if (y != z || k1 != k2) exit (__LINE__);
-
-    y = ldexp (x, 3);
-    z = ldexpf (x, 3);
-    if (y != z) exit (__LINE__);
-
-    y = exp (x);
-    z = expf (x);
-    if (y != z) exit (__LINE__);
-
-    y = cosh (x);
-    z = coshf (x);
-    if (y != z) exit (__LINE__);
-
-    y = sinh (x);
-    z = sinhf (x);
-    if (y != z) exit (__LINE__);
-
-    y = tanh (x);
-    z = tanhf (x);
-    if (y != z) exit (__LINE__);
-
-    y = acos (x);
-    z = acosf (x);
-    if (y != z) exit (__LINE__);
-
-    y = asin (x);
-    z = asinf (x);
-    if (y != z) exit (__LINE__);
-
-    y = atan (x);
-    z = atanf (x);
-    if (y != z) exit (__LINE__);
-
-    y = atan2 (x, 1.0f);
-    z = atan2f (x, 1.0f);
-    if (y != z) exit (__LINE__);
-
-    y = log (x);
-    z = logf (x);
-    if (y != z) exit (__LINE__);
-
-    y = log10 (x);
-    z = log10f (x);
-    if (y != z) exit (__LINE__);
-
-    y = pow (x, 3.0f);
-    z = powf (x, 3.0f);
-    if (y != z) exit (__LINE__);
+    CMP (log);
+    CMP (log10);
+    CMP (pow);
 
     i = isnan (x);
     if (isnanf (x) != i) exit (__LINE__);
@@ -150,42 +83,25 @@ int main ()
     i = isinf (x);
     if (isinff (x) != i) exit (__LINE__);
 
-#ifdef	__AVR__
     i = isfinite (x);
     if (isfinitef (x) != i) exit (__LINE__);
-#endif
 
     y = copysign (x, -1.0f);
     if (copysignf (x, -1.0f) != y) exit (__LINE__);
 
-#ifdef	__AVR__
     i = signbit (x);
     if (signbitf (x) != i) exit (__LINE__);
+
+    CMP (fdim);
+    CMP (fma);
+    CMP (fmax);
+    CMP (fmin);
+
+    CMP (trunc);
+    CMP (round);
+    CMP (lround);
+    CMP (lrint);
 #endif
-
-    y = fdim (x, -1.0f);
-    if (fdimf (x, -1.0f) != y) exit (__LINE__);
-
-    y = fma (x, 2.0f, 3.0f);
-    if (fmaf (x, 2.0f, 3.0f) != y) exit (__LINE__);
-
-    y = fmax (x, 0.0f);
-    if (fmaxf (x, 0.0f) != y) exit (__LINE__);
-
-    y = fmin (x, 1.0f);
-    if (fminf (x, 1.0f) != y) exit (__LINE__);
-
-    y = trunc (x);
-    if (truncf (x) != y) exit (__LINE__);
-
-    y = round (x);
-    if (roundf (x) != y) exit (__LINE__);
-
-    l = lround (x);
-    if (lroundf (x) != l) exit (__LINE__);
-
-    l = lrint (x);
-    if (lrintf (x) != l) exit (__LINE__);
 
     return 0;
 }
