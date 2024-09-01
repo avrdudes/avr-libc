01e527b08a1b116d9d7a57535b9205b75ae9b881
sep <sep@seplog.org>
2024-09-01 16:13:53 +0200

    define the type for sig_atomic_t. Do not create a variable and space for this

--- include/signal.h
+++ include/signal.h
@@ -12,7 +12,7 @@ extern sighandler_t signal(int signum, sighandler_t handler);
 #define SIG_HOLD ((sighandler_t)((void*)signal+2))
 #define SIG_IGN  ((sighandler_t)((void*)signal+3))
 
-volatile signed char sig_atomic_t;
+typedef signed char sig_atomic_t;
 typedef int sigset_t;
 
 extern int raise(int sig);
