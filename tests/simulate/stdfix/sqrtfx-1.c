0ce09d0eb36c92a99f5dd03784d87c496b3db937
Igor Proskurin <igor.v.proskurin@gmail.com>
2025-02-22 20:10:05 -0800

    stdfix-avrlibc.h: Initial support for fixed-point square root functions. #1024
    Add support for fixed-point square root functions using integer
    arithmetics.
    * The core function is __sqrthi() that computes the
      integer part of the square root of the 16-bit unsigned integer
      that has 8 bits of precision. This function is called internally
      to compute square roots of short fract types.
    * sqrtuhr() computes the square root of unsigned short fract.
      The result has 8-bits of precision.
    * sqrthr() computes the square root of short fract. The result
      has 7 bits of precision.

---
+++ tests/simulate/stdfix/sqrtfx-1.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/* Test selected values. */
+extern int __sqrthi(int arg);
+
+int main()
+{
+	int res = -1;
+	res = __sqrthi(0);
+	if (res != 0) exit(__LINE__);
+	res = __sqrthi(1);
+	if (res != 1) exit(__LINE__);
+	res = __sqrthi(4);
+	if (res != 2) exit(__LINE__);
+	res = __sqrthi(9);
+	if (res != 3) exit(__LINE__);
+	res = __sqrthi(16);
+	if (res != 4) exit(__LINE__);
+	res = __sqrthi(25);
+	if (res != 5) exit(__LINE__);
+	res = __sqrthi(225);
+	if (res != 15) exit(__LINE__);
+	res = __sqrthi(256);
+	if (res != 16) exit(__LINE__);
+	res = __sqrthi(625);
+	if (res != 25) exit(__LINE__);
+	res = __sqrthi(16129);
+	if (res != 127) exit(__LINE__);
+	res = __sqrthi(16383);
+	if (res != 127) exit(__LINE__);
+	return 0;
+}
