143b13fea9137bd761f09b3c87e19abb9342fa46
Igor Proskurin <105472369+sciprosk@users.noreply.github.com>
2025-03-24 10:37:04 -0700

    sqrtsi.S: Integer part of a square root of uint32_t. (#1029)
    Integer part of a square root of uint32_t returned as uint16_t
    using the algorithm for sqrt32_floor
    (https://www.mikrocontroller.net/articles/AVR_Arithmetik).  The
    same algorithm is used for devices with and without MUL.

---
+++ tests/simulate/stdfix/sqrtsi-1.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+extern uint16_t __sqrtsi(uint32_t);
+
+int main(void)
+{
+	uint16_t i;
+	uint16_t r;
+	uint32_t j;
+	uint32_t sqr;
+	if (UINT16_MAX != 65535U) exit(__LINE__);
+	if (UINT32_MAX != 4294967295UL) exit(__LINE__);
+	/* Test limits first. */
+	r = __sqrtsi(0UL);
+	if (r != 0U) exit(__LINE__);
+	r = __sqrtsi(1UL);
+	if (r != 1U) exit(__LINE__);
+	r = __sqrtsi(UINT32_MAX);
+	if (r != UINT16_MAX) exit(__LINE__);
+	/* Test all perfect squares. */
+	for (i = 1U; i < UINT16_MAX; i++){
+		sqr = (uint32_t)i * (uint32_t)i;
+		r = __sqrtsi(sqr);
+		if (r != i) exit(__LINE__);
+	}
+	/* Test rounding down for large values. */
+	sqr = (uint32_t)UINT16_MAX * (uint32_t)UINT16_MAX;
+	for (j = sqr; j < UINT32_MAX; j++) {
+		r = __sqrtsi(j);
+		if (r != UINT16_MAX) exit (__LINE__);
+	}
+	return 0;
+}
