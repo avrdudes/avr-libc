74e170dccff32e9fbc23b29aabc073e221bc3d42
Georg-Johann Lay <avr@gjlay.de>
2025-07-24 16:50:08 +0200

    onlinedocs: Make a "prerelease" of avr-libc-user-manual-2.3.0 on behalf of #1021

---
+++ docs/avr-libc-user-manual-2.3.0/examples/stdiodemo/defines.h
@@ -0,0 +1,27 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
+ * ----------------------------------------------------------------------------
+ *
+ * General stdiodemo defines
+ */
+
+/* CPU frequency */
+#define F_CPU 1000000UL
+
+/* UART baud rate */
+#define UART_BAUD  9600
+
+/* HD44780 LCD port connections */
+#define HD44780_RS A, 6
+#define HD44780_RW A, 4
+#define HD44780_E  A, 5
+/* The data bits have to be not only in ascending order but also consecutive. */
+#define HD44780_D4 A, 0
+
+/* Whether to read the busy flag, or fall back to
+   worst-time delays. */
+#define USE_BUSY_BIT 1
