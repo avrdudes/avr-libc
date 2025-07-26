31a92ee8af02d704cfc5a75e71167f3ec8d7852a
Georg-Johann Lay <avr@gjlay.de>
2025-07-26 17:39:23 +0200

    doc: time: Unify copyright notes.

--- libc/time/ctime.c
+++ libc/time/ctime.c
@@ -1,5 +1,5 @@
 /*
- * (C) 2012 Michael Duane Rice All rights reserved.
+ * Copyright (C) 2012 Michael Duane Rice All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
--- tests/simulate/time/tick.c
+++ tests/simulate/time/tick.c
@@ -1,5 +1,5 @@
 /*
- * (c)2012 Michael Duane Rice All rights reserved.
+ * Copyright (c) 2012 Michael Duane Rice All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
