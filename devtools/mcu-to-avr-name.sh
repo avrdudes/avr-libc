17ccb9f11578c141f3e4dca1172158387e2db8a3
Georg-Johann Lay <avr@gjlay.de>
2025-08-29 15:53:56 +0200

    fix: devtools/mcu-to-avr-name.sh: Make tr work on Solaris.

--- devtools/mcu-to-avr-name.sh
+++ devtools/mcu-to-avr-name.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Convert "atmega8" etc to the proper device spelling "ATmega8".
-tr a-z A-Z \
+tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ \
     | sed -e s/XMEGA/xmega/ -e s/MEGA/mega/ -e s/TINY/tiny/ -e s/AUTO/auto/
