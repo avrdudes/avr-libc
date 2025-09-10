b357b0c3f28454c0d092171dfeaccde3ba716d58
Georg-Johann Lay <avr@gjlay.de>
2025-09-10 18:21:03 +0200

    doc: doc/api/Makefile.am: Remove demo from demodox (ad #1048).
    examples/demo has no more images, hence tidy up the dox target.

--- doc/examples/demo/avrlibc.mk
+++ doc/examples/demo/avrlibc.mk
@@ -1,23 +1,3 @@
-FIG2DEV                 = fig2dev
-EXTRA_CLEAN_FILES       = *.hex *.bin *.srec
-
-.PHONY: dox eps png pdf
-
-dox: eps png pdf
-
-eps: $(PRG).eps
-png: $(PRG).png
-pdf: $(PRG).pdf
-
-%.eps: %.fig
-	$(FIG2DEV) -L eps $< $@
-
-%.pdf: %.fig
-	$(FIG2DEV) -L pdf $< $@
-
-%.png: %.fig
-	$(FIG2DEV) -L png $< $@
-
 # In order to align demo.dox with demo.map, generate a small file
 # that contains the .text size.
 
