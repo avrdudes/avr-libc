709b82b0909a6a7e660532d4db864addb5166fa0
Georg-Johann Lay <avr@gjlay.de>
2025-09-13 22:44:46 +0200

    hack: avr/eeprom-4.c: Work around SimulAVR bug 66044 #954.
    https://savannah.nongnu.org/bugs/?66044

--- tests/simulate/avr/eeprom-4.c
+++ tests/simulate/avr/eeprom-4.c
@@ -5,6 +5,21 @@
 
 #define VAL 0x1122334455667788
 
+/* Work around issue #954, which is due to a bug in SimulAVR.
+   Clear XL by hand prior to calling the very EEPROM read func. */
+#ifdef __AVR_ATtiny2313__
+extern uint32_t eer_dword (const uint32_t*);
+extern uint64_t eer_qword (const uint64_t*);
+#define eeprom_read_dword(x) eer_dword (x)
+#define eeprom_read_qword(x) eer_qword (x)
+__asm (".pushsection .text\n"
+       ".global eer_dword\n"
+       ".global eer_qword\n"
+       "eer_dword: clr r27 $ rjmp eeprom_read_dword\n"
+       "eer_qword: clr r27 $ rjmp eeprom_read_qword\n"
+       ".popsection");
+#endif
+
 void test1 (void)
 {
   eeprom_write_qword (ADDR, VAL);
