46773d88332ba79e68c75620a971ca149f074611
Georg-Johann Lay <avr@gjlay.de>
2025-09-28 18:02:53 +0200

    doc: examples/demo: Show program size and how to get it.

--- doc/examples/demo/avrlibc.mk
+++ doc/examples/demo/avrlibc.mk
@@ -15,3 +15,22 @@ vectorssize.txt: $(PRG).elf
 	echo "$(shell $V) = $(shell echo $$((`$V`)))" > $@
 
 EXTRA_CLEAN_FILES += textsize.txt  vectorssize.txt
+
+
+# Showing the size of the program.  Some extra salt is needed to get
+# a nice output (no trailing white lines), and to graceful degrade
+# when avr-objdump -P mem-usage doesn't work for whatever reason.
+# Therefore, we put the following rule in this file and not in Makefile.
+
+all: $(PRG).size
+
+SED = sed
+
+%.size: %.elf
+	$(OBJDUMP) -P mem-usage $< > $@ \
+		|| echo "error: $(OBJDUMP) -P mem-usage $< failed" > $@
+	$(SED) -i '$${/^$$/d}' $@
+	$(SED) -i '$${/^$$/d}' $@
+	$(SED) -i '$${/^$$/d}' $@
+
+EXTRA_CLEAN_FILES += $(PRG).size
