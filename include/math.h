fee31aa07c902fb84b8211dd6e0b21099f722d90
Georg-Johann Lay <avr@gjlay.de>
2025-10-06 22:26:58 +0200

    doc: math.h: Add some info about the IEEE double sincos().

--- include/math.h
+++ include/math.h
@@ -189,12 +189,17 @@ void sincosf (float x, float *psin, float *pcos);
 
 /** The sincos() function returns the sine of \a x in \c *psin, and
     the cosine of \a x in \c *pcos.  The angle \a x is measured in radians.
-    \since AVR-LibC v2.3 */
+    As an example, the performace gain of the IEEE double version of
+    sincos() compared to a sin() plus a cos() call is around 5000 cycles.
+    \since GCC v15.3 (IEEE double), AVR-LibC v2.3 */
 void sincos (double x, double *psin, double *pcos);
 
 /** The sincosl() function returns the sine of \a x in \c *psin, and
     the cosine of \a x in \c *pcos.  The angle \a x is measured in radians.
-    \since AVR-LibC v2.3 */
+    As an example, the performace gain of the IEEE double version of
+    sincosl() compared to a sinl() plus a cosl() call is around 5000 cycles.
+    for \a x = 2.0.
+    \since GCC v15.3 (IEEE double), AVR-LibC v2.3 */
 void sincosl (long double x, long double *psin, long double *pcos);
 
 /** The tanf() function returns the tangent of \a x, measured in radians. */
