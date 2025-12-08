0f78a7750a5028b9c19cfb3a19f2e9ac10ec9d5c
Georg-Johann Lay <avr@gjlay.de>
2025-12-08 18:42:35 +0100

    add: tests/bench-rand: New folder with RNG benchmarking.

--- include/stdlib.h
+++ include/stdlib.h
@@ -527,11 +527,10 @@ extern long double atofl(const char *__nptr);
      If no seed value is provided, the functions are automatically seeded with
      a value of 1.
 
-     rand() achieves a score of 73% in the \c bbattery_SmallCrush tests from the
+     rand() achieves a score of 80% in the \c bbattery_SmallCrush tests from the
      <a href="https://simul.iro.umontreal.ca/testu01/tu01.html">TestU01</a>
      suite.  For a better score consider random(), which achieves 100% in
-     that test. (Using only the lower 15 bits of random() achieves a
-     rating of 83%).
+     that test.
 
      For the resource consumptions, see the \ref bench_libc "libc benchmarks".
 */
